e51a12698964b33acf1b26bcb7c18da11aa29f6c
Pablo Martin <caedes@sindominio.net>
2011-02-01 09:39:51 +0100

    bump version numbers and store application in exporter to avoid losing state on undo (experimental).

--- scripts/realxtend_exporter.py
+++ scripts/realxtend_exporter.py
@@ -8,7 +8,7 @@ Tooltip: 'Exports the current scene to RealXtend server'
 """
 
 __author__ = ['Pablo Martin']
-__version__ = '0.2'
+__version__ = '0.8'
 __url__ = ['B2rex Sim, http://sim.lorea.org',
            'B2rex forum, https://sim.lorea.org/pg/groups/5/b2rex/',
 	   'B2rex repo, http://github.com/caedesvvv/b2rex']
@@ -20,6 +20,10 @@ from b2rexpkg.app import RealxtendExporterApplication
 
 
 if __name__ == "__main__":
-    application = RealxtendExporterApplication()
+    if hasattr(b2rexpkg, "application"):
+        application = b2rexpkg.application
+    else:
+        application = RealxtendExporterApplication()
+        b2rexpkg.application = application
     application.go()
 
--- scripts/realxtend_materials.py
+++ scripts/realxtend_materials.py
@@ -8,7 +8,7 @@ Tooltip: 'See material properties from objects'
 """
 
 __author__ = ['Pablo Martin']
-__version__ = '0.2'
+__version__ = '0.8'
 __url__ = ['B2rex Sim, http://sim.lorea.org',
            'B2rex forum, https://sim.lorea.org/pg/groups/5/b2rex/',
 	   'B2rex repo, http://github.com/caedesvvv/b2rex']
