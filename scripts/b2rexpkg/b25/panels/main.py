e1089de04eb3765e1d853f8681af563ebff97c95
Pablo Martin <caedes@sindominio.net>
2011-02-10 06:36:34 +0100

    cleaning up interface code a bit.

--- scripts/b2rexpkg/b25/panels/main.py
+++ scripts/b2rexpkg/b25/panels/main.py
@@ -30,17 +30,7 @@ class ConnectionPanel(bpy.types.Panel):
         pass # we dont really want this callback, but keeps the object
         # from dying all the time so we keep it for now
 
-    def draw(self, context):
-        if self.cb_view == None:
-            # callback is important so object doesnt die ?
-            self.cb_view = context.region.callback_add(self.draw_callback_view, 
-                                        (context, ),
-                                        'POST_VIEW') # XXX POST_PIXEL ;)
-        
-        layout = self.layout
-        props = context.scene.b2rex_props
-        session = bpy.b2rex_session
-
+    def draw_connection_panel(self, layout, session, props):
         box = layout.box()
         if session.connected and session.rt_support:
             box_c = box.row()
@@ -59,14 +49,10 @@ class ConnectionPanel(bpy.types.Panel):
         else:
             box.operator("b2rex.connect", text="Connect")
         box.operator("b2rex.export", text="Export")
-        row = layout.row()
-        row = box.row() 
-        row.label(text="Status: "+session.status)
 
+        box.label(text="Status: "+session.status)
 
-        if len(props.regions):
-            row = layout.row() 
-            row.template_list(props, 'regions', props, 'selected_region')
+    def draw_chat(self, layout, session, props):
         if len(props.chat):
             row = layout.row() 
             row.label(text="Chat")
@@ -78,28 +64,39 @@ class ConnectionPanel(bpy.types.Panel):
                 props.next_chat = ""
             row = layout.row()
             row.prop(props, 'next_chat')
+
+    def draw(self, context):
+        if self.cb_view == None:
+            # callback is important so object doesnt die ?
+            self.cb_view = context.region.callback_add(self.draw_callback_view, 
+                                        (context, ),
+                                        'POST_VIEW') # XXX POST_PIXEL ;)
+        
+        layout = self.layout
+        props = context.scene.b2rex_props
+        session = bpy.b2rex_session
+
+        self.draw_connection_panel(layout, session, props)
+
+        if len(props.regions):
+            row = layout.row() 
+            row.template_list(props, 'regions', props, 'selected_region')
+
+        self.draw_chat(layout, session, props)
+
         if props.selected_region > -1:
-            box = layout.row()
-            col = box.column()
+            col = layout.column_flow(0)
             col.operator("b2rex.exportupload", text="Export/Upload")
-            col = box.column()
             col.operator("b2rex.export", text="Upload")
-            box = layout.row()
-            col = box.column()
             col.operator("b2rex.export", text="Clear")
-            col = box.column()
             col.operator("b2rex.check", text="Check")
-            box = layout.row()
-            col = box.column()
             col.operator("b2rex.sync", text="Sync")
-            col = box.column()
             col.operator("b2rex.import", text="Import")
 
-        row = layout.row()
+        row = layout.column()
 
         for k in session.region_report:
             row.label(text=k)
-            row = layout.row()
 
         self.draw_stats(layout, session, props)
         self.draw_settings(layout, session, props)
@@ -112,25 +109,18 @@ class ConnectionPanel(bpy.types.Panel):
             box = layout.box()
             if session.simrt:
                 if session.agent_id:
-                    row = box.row()
-                    row.label(text="agent: "+session.agent_id+" "+session.agent_access)
+                    box.label(text="agent: "+session.agent_id+" "+session.agent_access)
 
-            row = box.row()
-            row.label(text="cmds in: %d out: %d updates: %d"%tuple(session.stats[:3]))
-            row = box.row() 
-            row.label(text="http requests: %d ok: %d"%tuple(session.stats[3:5]))
-            row = box.row() 
-            row.label(text="queue pending: %d last time: %d"%tuple(session.stats[5:7])+" last sec: "+str(session.second_budget))
-            row = box.row() 
-            row.label(text="threads workers: "+str(session.stats[7]))
-            row = box.row() 
-            row.label(text="updates cmd: %d view: %d"%tuple(session.stats[8:10]))
+            box.label(text="cmds in: %d out: %d updates: %d"%tuple(session.stats[:3]))
+            box.label(text="http requests: %d ok: %d"%tuple(session.stats[3:5]))
+            box.label(text="queue pending: %d last time: %d"%tuple(session.stats[5:7])+" last sec: "+str(session.second_budget))
+            box.label(text="threads workers: "+str(session.stats[7]))
+            box.label(text="updates cmd: %d view: %d"%tuple(session.stats[8:10]))
         else:
             row.prop(props,"show_stats", icon="TRIA_RIGHT", text="Stats", emboss=False)
 
 
     def draw_settings(self, layout, session, props):
-        box = layout.row()
         row = layout.row()
         if not props.expand:
             row.prop(props,"expand", icon="TRIA_DOWN", text="Settings", emboss=False)
@@ -139,17 +129,13 @@ class ConnectionPanel(bpy.types.Panel):
                 row = layout.row()
                 row.prop(props, prop)
 
-            box = layout.row()
-            col = box.column()
+            col = layout.column_flow()
             col.prop(props,"regenMaterials")
-            col = box.column()
             col.prop(props,"regenObjects")
 
-            box = layout.row()
-            col = box.column()
             col.prop(props,"regenTextures")
-            col = box.column()
             col.prop(props,"regenMeshes")
+
             box = layout.row()
             box.operator_menu_enum("b2rex.loglevel", 'level', icon='INFO',
                                    text=getLogLabel(props.loglevel))
