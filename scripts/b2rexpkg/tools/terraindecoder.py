2a7ad7dd3c6addb8856ef8cf6952da60ffd53bcd
Pablo Martin <caedes@sindominio.net>
2011-02-10 15:52:58 +0100

    add a bit reader copied from naali.

--- scripts/b2rexpkg/tools/terraindecoder.py
+++ scripts/b2rexpkg/tools/terraindecoder.py
@@ -1,9 +1,56 @@
+import sys
 import os
 import math
 import struct
 import traceback
 
 from bitstring import ConstBitStream
+
+class BitReader(object):
+    _num_bits_in_elem = 8
+    def __init__(self, data):
+        self._bit_ofs = 0
+        self._elem_ofs = 0
+        self._data = data
+        self.len = len(data)*8
+        self.pos = 0
+    def ReadBits(self, count):
+        data = [0,0,0,0]
+        cur_byte = 0
+        cur_bit = 0
+        total_bits = min(self._num_bits_in_elem, count)
+        while count > 0:
+            bit = self.ReadBit()
+            if bit:
+                newval = data[cur_byte] | (1 << (total_bits - 1 - cur_bit))
+                data[cur_byte] = newval
+            cur_bit += 1
+            if cur_bit >= self._num_bits_in_elem:
+                cur_byte += 1
+                cur_bit = 0
+                total_bits = min(self._num_bits_in_elem, count)
+            count -= 1
+        return struct.pack("<BBBB", *data)
+    def read(self, instructions):
+        if instructions in ["bool"]:
+            return self.ReadBit()
+        datatype, nbits = instructions.split(":")
+        if datatype in ["uintle", "uint"]:
+            return struct.unpack("<I", self.ReadBits(int(nbits)))[0]
+        if datatype in ["floatle", "float"]:
+            return struct.unpack("<f", self.ReadBits(32))[0]
+    def BitsLeft(self):
+        return self.len - self.pos
+    def ReadBit(self):
+        if self.BitsLeft() == 0:
+            raise Exception("Out of bits!")
+        bit = struct.unpack("<B", self._data[self._elem_ofs])[0] & (1 << (self._num_bits_in_elem - 1 - self._bit_ofs)) != 0
+        self._bit_ofs += 1
+        if self._bit_ofs >= self._num_bits_in_elem:
+            self._bit_ofs = 0
+            self._elem_ofs += 1
+        self.pos = self._bit_ofs + (self._elem_ofs*8)
+        return bit
 
 cEndOfPatches = 97;
 OO_SQRT2 = 0.7071067811865475244008443621049
@@ -69,24 +116,30 @@ class IDCTPrecomputationTables(object):
 precompTables = IDCTPrecomputationTables(16)
 
 class PatchHeader(object):
-    def __init__(self, data, patchSize, rawdata):
+    def __init__(self, patchSize, data=None, rawdata=None):
         self.patchSize = patchSize
-        self.decode(data, rawdata)
+        if data:
+            self.decode(data, rawdata)
     def decode(self, data, rawdata):
         self.quantWBits = data.read("uintle:8")
         if self.quantWBits == cEndOfPatches:
             return
         self.dcOffset = data.read("floatle:32")
-        self.range = data.read("uint:16")
+        self.range = data.read("uintle:16")
+        #uintpos = data.pos
+        #x = data.read("uint:5")
+        #y = data.read("uint:5")
+        #data.seek(uintpos)
         patchIDs = data.read("uint:10")
-        #patchIDs = struct.unpack("<H", struct.pack(">H", patchIDs))[0]
         x = patchIDs >> 5
         y = patchIDs & 31
+        patchIDs = struct.unpack("<H", struct.pack(">H", patchIDs))[0]
+        x2 = patchIDs >> 5
+        y2 = patchIDs & 31
+        print("LAND", bin(x), bin(y), bin(x2), bin(y2))
         self.wordBits = (self.quantWBits & 0x0f) + 2
         print("Block", self.dcOffset, self.range, "x", x,
               "y", y, self.wordBits, data.pos)
-        assert(x < self.patchSize)
-        assert(y < self.patchSize)
         self.x = x
         self.y = y
 
@@ -100,7 +153,7 @@ class TerrainDecoder(object):
             data = data[4:]
             print("stride",stride,"patchSize" ,patchSize,"layerType", layerType,
                  len(data), len(data)*8)
-        self.decompressLand(data, stride, patchSize)
+        self.decompressLand(data, stride, patchSize, layerType)
     @staticmethod
     def decode(data, stride=None, patchSize=None):
         decoder = TerrainDecoder(data, stride, patchSize)
@@ -111,7 +164,7 @@ class TerrainDecoder(object):
         patchdata = list(range(size*size))
         for i in range(size*size):
             if data.len - data.pos <= 0:
-                print("out of bits when decoding terrain vertex", i, bitsleft)
+                print("out of bits when decoding terrain vertex")
                 while i < size*size:
                     patchdata[i] = 0
                     i+=1
@@ -176,44 +229,80 @@ class TerrainDecoder(object):
                 total += linein[lineSize + u] * precompTables.cosineTable[u *16 +  n]
             lineout[lineSize+n] = total*oosob
 
-    def decompressLand(self, rawdata, stride, patchSize):
+    def decompressLand(self, rawdata, stride, patchSize, layerType):
         #data = dec2bin(rawdata)
-        data = ConstBitStream(bytes=rawdata, length=len(rawdata)*8)
-        while data.pos < len(data):
+        #data = ConstBitStream(bytes=rawdata, length=len(rawdata)*8)
+        data = BitReader(rawdata)
+        iter = 0
+        header = PatchHeader(patchSize)
+        while data.BitsLeft() > 0:
             try:
-                header = self.decodePatchHeader(data, patchSize, rawdata)
+                header = self.decodePatchHeader(data, patchSize, rawdata, header)
             except:
-                print("TerrainDecoder:DecompressLand: Invalid header data!",
-                        data.pos)
                 traceback.print_exc()
+                print("LAND:DecompressLand: Invalid header data!",
+                        data.BitsLeft(), layerType, patchSize, stride, header.x,
+                      header.y, iter)
                 return
             if header.quantWBits == cEndOfPatches:
-                print("LAND OK", len(self.patches), patchSize, stride)
+                print("LAND OK", len(self.patches), patchSize, stride,
+                      layerType, data.BitsLeft(), data, header.x, header.y,
+                      header.range, header.dcOffset)
                 return
             cPatchesPerEdge = 16 # patchSize ?
             if header.x >= cPatchesPerEdge or header.y >= cPatchesPerEdge:
-                print("TerrainDecoder:DecompressLand: Invalid patch data!",
-                      data.pos)
+                print("LAND:DecompressLand: Invalid patch data!",
+                      data.BitsLeft(), layerType, header.x, header.y, iter)
                 return
             patch = self.decodeTerrainPatch(header, data, patchSize)
             patch = self.decompressTerrainPatch(header, patch)
             self.patches.append([header, patch])
+            iter += 1
             print("next iter",data.pos)
-    def decodePatchHeader(self, data, patchSize, rawdata):
-        header = PatchHeader(data, patchSize, rawdata)
+    def decodePatchHeader(self, data, patchSize, rawdata, header=None):
+        if header:
+            header.decode(data, rawdata)
+        else:
+            header = PatchHeader(patchSize, data, rawdata)
         return header
 
+
+def checkbitreader():
+    a = b''
+    x1 = 10
+    x2 = 245
+    x3 = 666.0
+    a = struct.pack("<f",x1)
+    a += struct.pack("<f",x2)
+    a += struct.pack("<f",x3)
+    a += struct.pack("<I",20)
+    a += struct.pack("<H",10)
+    a += struct.pack("<B",255)
+    a += struct.pack("<B",0)
+    bits = BitReader(a)
+    assert(bits.read("float:32") == x1)
+    assert(bits.read("float:32") == x2)
+    assert(bits.read("float:32") == x3)
+    assert(bits.read("uint:32") == 20)
+    assert(bits.read("uint:16") == 10)
+    for i in range(8):
+        assert(bits.read("bool") == True)
+    for i in range(8):
+        assert(bits.read("bool") == False)
+    assert(bits.len == 32*4+16+16)
+    assert(bits.pos == bits.len)
 
 if __name__ == "__main__":
     b = os.path.dirname
     scriptdir = os.path.realpath(__file__)
+    checkbitreader()
     layerfolder = os.path.join(b(b(b(b(scriptdir)))), "test", "layers")
     for layer_file in os.listdir(layerfolder):
-        #if not layer_file == "1.layer":
-            #    continue
+        #if not layer_file == "0.layer":
+            #        continue
         print("DECODING", layer_file)
         f = open(os.path.join(layerfolder, layer_file), "rb")
         data = f.read()
         f.close()
         res = TerrainDecoder.decode(data)
-        #print("RESULT",len(res))
+        print("RESULT", len(res), res)
