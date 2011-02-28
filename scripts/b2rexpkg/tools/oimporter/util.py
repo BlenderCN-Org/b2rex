57e64ddcb1ea27574aa5fdac4de6af353c6f8025
Pablo Martin <caedes@sindominio.net>
2011-02-28 03:41:53 +0100

    dont miss on missing data.

--- scripts/b2rexpkg/tools/oimporter/util.py
+++ scripts/b2rexpkg/tools/oimporter/util.py
@@ -20,10 +20,15 @@ def get_nor(idx, vbuffer, no_offset):
     no2 = arr2float(v[no_offset+4:no_offset+8])
     no3 = arr2float(v[no_offset+8:no_offset+12])
     return (no1, no2, no3)
+
 def get_uv(idx, vbuffer, uv_offset):
-    v = vbuffer[idx*32:(idx+1)*32]
-    uv1 = arr2float(v[uv_offset+0:uv_offset+4])
-    uv2 = arr2float(v[uv_offset+4:uv_offset+8])
+    if len(vbuffer) >= (idx+1)*32:
+        v = vbuffer[idx*32:(idx+1)*32]
+        uv1 = arr2float(v[uv_offset+0:uv_offset+4])
+        uv2 = arr2float(v[uv_offset+4:uv_offset+8])
+    else:
+        uv1 = 0.0
+        uv2 = 1.0
     return (uv1, 1.0-uv2)
 
 def mat_findtextures(material):
@@ -44,7 +49,7 @@ def mat_findtextures(material):
 
 def get_vcoords(vbuffer, idx, pos_offset, stride=32):
     v = vbuffer[idx*stride:(idx+1)*stride]
-    if v:
+    if v and len(v) >= pos_offset+12:
         v1 = arr2float(v[pos_offset+0:pos_offset+4])
         v2 = arr2float(v[pos_offset+4:pos_offset+8])
         v3 = arr2float(v[pos_offset+8:pos_offset+12])
