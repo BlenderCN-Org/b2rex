ed2ba970eac57f7fdf661f8a8786d592abfe3dda
Pablo Martin <caedes@sindominio.net>
2011-04-02 20:33:06 +0200

    adding a method for VLE-1.7/1.7/16 encoding.

--- scripts/b2rexpkg/tools/knet/data.py
+++ scripts/b2rexpkg/tools/knet/data.py
@@ -9,6 +9,21 @@ class KristalliData(object):
         self._dyn_matrix[16] = self.get_u16
         self._dyn_matrix[32] = self.get_u32
 
+    @staticmethod
+    def encode_ve16(num):
+        if num < 128:
+            return struct.pack("<B", num)
+        elif num < 16384:
+            c = (num & 127) | 128
+            b = num>>7
+            encoded = struct.pack("<B", c) + struct.pack("<B", b)
+            return encoded
+        else:
+            # XXX TODO
+            print("KristalliData:encode_ve16:unimplemented_path")
+            encoded = struct.pack("<I", num)
+            return encoded
+
     def get_u8(self):
         val = struct.unpack('<B', self._data[self._idx:self._idx+1])[0]
         self._idx += 1
