e6095c6bcc587ad7df839c3ae3360f7791f83b5b
Pablo Martin <caedes@sindominio.net>
2011-04-09 22:07:08 +0200

    allow xmlrpc connection to stay open.

--- scripts/b2rexpkg/tools/simconnection.py
+++ scripts/b2rexpkg/tools/simconnection.py
@@ -8,8 +8,43 @@ logger = logging.getLogger('b2rex.simconnection')
 
 if sys.version_info[0] == 2:
     import xmlrpclib
+    import httplib
 else:
     import xmlrpc.client as xmlrpclib
+    import http.client as httplib
+
+class PersistTransport(xmlrpclib.Transport):
+    '''Provides a Transport for the xmlrpclib that uses httplib
+    supporting persistent connections
+    Does not close the connection after each request.
+    '''
+    connection = None
+
+    def request(self, host, handler, request_body, verbose=0):
+        if not self.connection:
+            host, extra_headers, x509 = self.get_host_info(host)
+            self.connection = httplib.HTTPConnection(host)
+            self.headers = {"User-Agent" : self.user_agent,
+                           "Content-Type" : "text/xml",
+                            "Accept": "text/xml"}
+            if extra_headers:
+                for key, item in extra_headers:
+                    self.headers[key] = item
+
+            self.headers["Content-Length"] = str(len(request_body))
+            self.connection.request('POST', handler, request_body,
+            self.headers)
+            r = self.connection.getresponse()
+            if r.status != 200:
+                self.connection.close()
+                self.connection = None
+                raise xmlrpclib.ProtocolError( host + handler, r.status,
+            r.reason, '' )
+            data = r.read()
+            p, u = self.getparser()
+            p.feed(data)
+            p.close()
+            return u.close()
 
 class SimConnection(object):
     def __init__(self):
@@ -21,7 +56,7 @@ class SimConnection(object):
         """
         Connect to the xmlrpc server.
         """
-        self._con = xmlrpclib.Server(url)
+        self._con = xmlrpclib.Server(url, transport=PersistTransport())
 
     def login(self, first, last, passwd):
         """
@@ -60,8 +95,8 @@ class SimConnection(object):
 
 if __name__ == "__main__":
     con = SimConnection()
-    logger.debug(con.connect("https://sim.lorea.org/xml-rpc.php"))
-    login_info = con.login("caedes", "caedes", "nemesis")
+    logger.debug(con.connect("http://taigalife.ath.cx:8002"))
+    login_info = con.login("Blender", "b2rex", "B2rex")
     print(login_info["sim_ip"], login_info["sim_port"])
     sys.exit()
     logger.debug(con.connect("http://10.66.66.79:8002"))
